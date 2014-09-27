b487b897d823aa7c83228c907714e225135f2fe2
Francesc Elies Henar <felies@visualfabriq.com>
2014-09-27 18:20:42 +0200

    ctable groupby

--- bcolz/ctable_groupby_test.py
+++ bcolz/ctable_groupby_test.py
@@ -1,3 +1,37 @@
+# accounts = [
+#     (1, 'Alice',   100, 'F', 'AMS',  1),  # id, name, balance, gender
+#     (2, 'Bob',     200, 'M', 'AMS',  2),
+#     (3, 'Charlie', 150, 'M', 'ROT',  4),
+#     (4, 'Dennis',   50, 'M', 'AMS',  8),
+#     (5, 'Edith',   300, 'F', 'AMS', 16),
+# ]
+#
+# from toolz import groupby, valmap, compose
+# from toolz.curried import get, pluck
+#
+# binop = lambda total, (id, name, bal, gend): total + bal
+#
+# gb = groupby(get(3), accounts)
+# # {
+# #  'F': [(1, 'Alice', 100, 'F'), (5, 'Edith', 300, 'F')],
+# #  'M': [(2, 'Bob', 200, 'M'), (3, 'Charlie', 150, 'M'), (4, 'Dennis', 50, 'M')]
+# # }
+# valmap(compose(sum, pluck(2)), gb)
+# # {'F': 400, 'M': 400}
+#
+# pipe(accounts, groupby(get(3)), valmap(compose(sum, pluck(2))))
+#
+# # -----
+#
+# import pandas as pd
+# df = pd.DataFrame({'col1':['A','A','A','B','B','B'], 'col2':['C','D','D','D','C','C'], 'col3':[.1,.2,.4,.6,.8,1]})
+# def func3(df):
+#     dfout =  sum(df['col3']**2)
+#     return  dfout
+# t = df.groupby(['col1', 'col2']).apply(func3)
+#
+# # -----------------------
+#
 # from random import randint
 # import numpy as np
 # A = np.array([1.1, 1.1, 3.3, 3.3, 5.5, 6.6])
@@ -11,21 +45,20 @@
 
 # ------
 
-from prettyprint import pp
 import bcolz
 import tempfile
 import os
 import pandas as pd
 
 projects = [
-    {'name': 'build roads',  'state': 'CA', 'cost':   1, 'cost2':  2, 'cost3':    3},
-    {'name': 'fight crime',  'state': 'IL', 'cost':   2, 'cost2':  3, 'cost3':    4},
-    {'name': 'help farmers', 'state': 'IL', 'cost':   4, 'cost2':  5, 'cost3':    6},
-    {'name': 'help farmers', 'state': 'CA', 'cost':   8, 'cost2':  9, 'cost3':   10},
-    {'name': 'build roads',  'state': 'CA', 'cost':  16, 'cost2': 17, 'cost3':   18},
-    {'name': 'fight crime',  'state': 'IL', 'cost':  32, 'cost2': 33, 'cost3':   34},
-    {'name': 'help farmers', 'state': 'IL', 'cost':  64, 'cost2': 65, 'cost3':   66},
-    {'name': 'help farmers', 'state': 'AR', 'cost': 128, 'cost2': 129, 'cost3': 130}
+    {'name': 'build roads',  'state': 'CA', 'cost': 1000000, 'cost2': 1000000},
+    {'name': 'fight crime',  'state': 'IL', 'cost': 100000,  'cost2': 100000},
+    {'name': 'help farmers', 'state': 'IL', 'cost': 2000000, 'cost2': 2000000},
+    {'name': 'help farmers', 'state': 'CA', 'cost': 200000,  'cost2': 200000},
+    {'name': 'build roads',  'state': 'CA', 'cost': 9000000, 'cost2': 199900},
+    {'name': 'fight crime',  'state': 'IL', 'cost': 900000,  'cost2': 19990},
+    {'name': 'help farmers', 'state': 'IL', 'cost': 9000000, 'cost2': 299900},
+    {'name': 'help farmers', 'state': 'AR', 'cost': 900000,  'cost2': 29990}
 ]
 
 df_tmp = pd.DataFrame(projects)
@@ -37,22 +70,9 @@ print df
 prefix = 'bcolz-TestH5'
 rootdir = tempfile.mkdtemp(prefix=prefix)
 os.rmdir(rootdir)  # tests needs this cleared
+print(rootdir)
 fact_bcolz = bcolz.ctable.fromdataframe(df, rootdir=rootdir)
 print '--'
-res1 = fact_bcolz.groupby(
-    ['state'],
-    [{'sum_costs':{'sum': ['cost', 'cost2']}}]
-)
-pp(res1)
+fact_bcolz.groupby(['state','name'], {'sum_costs':['sum', 'cost', 'cost2']})
 print '--'
-
-res2 = fact_bcolz.groupby(
-    ['state', 'name'],
-    [
-        {'sum_cost1':{'sum': ['cost']}},
-        {'sum_cost2':{'sum': ['cost', 'cost2']}},
-        {'sum_costs':{'sum': ['cost', 'cost2', 'cost3']}}
-    ]
-)
-
-pp(res2)
+# fact_bcolz.groupby(['state'],{'sum_costs':['sum', 'cost', 'cost2']})
