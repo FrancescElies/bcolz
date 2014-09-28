4e10dc86d103ca2437419dce8cc9f0d53f589cae
CarstVaartjes <cvaartjes@visualfabriq.com>
2014-09-28 19:10:22 +0200

    groupby Cython <- now using cytoolz specialized variant

--- bcolz/ctable_ext.pyx
+++ bcolz/ctable_ext.pyx
@@ -1,4 +1,39 @@
-%load_ext cythonmagic
+import numpy as np
+cimport numpy as np
+import cython
+import bcolz
+from cpython.ref cimport PyObject, Py_DECREF, Py_INCREF, Py_XDECREF
+from cpython cimport (PyDict_New, PyDict_GetItem, PyDict_SetItem,
+                      PyDict_Contains, PyDict_Keys,
+                      PyTuple_SET_ITEM,
+                      PyList_Check, PyList_GET_SIZE, PyFloat_Check,
+                      PyString_Check,
+                      PyBytes_Check,
+                      PyTuple_SetItem,
+                      PyTuple_New,
+                      PyObject_SetAttrString)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef inline object _dict_update(dict d, tuple key, tuple input_val):
+    cdef PyObject *obj = PyDict_GetItem(d, key)
+    cdef tuple current_val, new_val
+    cdef Py_ssize_t i, N
+    if obj is NULL:
+        PyDict_SetItem(d, key, input_val)
+    else:
+        current_val = <object>obj
+        N = PyList_GET_SIZE(input_val)
+        new_val = PyTuple_New(N)
+        for i in range(N):
+            row_val = current_val[i] + input_val[i]
+            Py_INCREF(row_val)
+            PyTuple_SET_ITEM(new_val, i, row_val)
+        PyDict_SetItem(d, key, new_val)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef groupby_cython2(input_ctable, list groupby_cols, list measure_cols):
     """
     Groups the measure_cols over the groupby_cols. Currently only sums are supported.
     NB: current Python standard hash *might* have collisions!
@@ -7,101 +42,47 @@
     :param measure_cols: A list of measure columns (sum only atm)
     :return:
     """
-
-%timeit df.groupby(['state'])['cost', 'cost2'].sum()
-%timeit groupby_cython(fact_bcolz, ['state'], ['cost', 'cost2'])
-
-input_ctable = fact_bcolz
-groupby_cols = ['state']
-measure_cols = ['cost', 'cost2']
-
-%%cython
-import numpy as np
-cimport numpy as np
-import cython
-import bcolz
-from cpython cimport (PyDict_New, PyDict_GetItem, PyDict_SetItem,
-                      PyDict_Contains, PyDict_Keys,
-                      Py_INCREF, PyTuple_SET_ITEM,
-                      PyList_Check, PyFloat_Check,
-                      PyString_Check,
-                      PyBytes_Check,
-                      PyTuple_SetItem,
-                      PyTuple_New,
-                      PyObject_SetAttrString)
-@cython.boundscheck(False)
-@cython.wraparound(False)
-cpdef groupby_cython(input_ctable, list groupby_cols, list measure_cols):
-    cdef int arr_len, i, j, col_nr, hash_max, \
-        current_hash, previous_hash, \
+    cdef int arr_len, actual_len, i, j, col_nr, hash_max, \
         current_index, previous_index, \
         groupby_cols_len, measure_cols_len
     cdef str col
     # cdef tuple row <- namedtuple issue?
-    cdef list outcols, groupby_matrix, measure_matrix, total_matrix
-    cdef object tup
+    cdef list outcols, total_matrix
+    cdef object row
     cdef bint hash_found
     cdef np.ndarray hash_arr, output_arr
-    cdef tuple groupby_tup, measure_tup, groupby_matrix_tup, measure_matrix_tup
-    # cdef bcolz.carray_ext.carray index_arr, hash_arr, current_col <- array issue
+    cdef tuple groupby_tup, measure_tup
+    cdef dict total_dict
     arr_len = input_ctable.size
-    # Make a new type hash array: this will have a hash code of the group by columns
-    hash_arr = np.zeros(arr_len, dtype=int)  # bcolz.carray()
-    # Make an integer variable hash_max that starts at 0
+    total_dict = {}
     hash_max = 0
-    previous_hash = 0
     current_index = 0
     outcols = groupby_cols + measure_cols
-    # prepare groupby part
     groupby_cols_len = len(groupby_cols)
-    groupby_matrix = []
-    for col in groupby_cols:
-        output_arr = np.zeros(arr_len, input_ctable.dtype[col])
-        groupby_matrix.append(output_arr)
-    groupby_matrix_tup = tuple(groupby_matrix)
-    # prepare measure part
     measure_cols_len = len(measure_cols)
-    measure_matrix = []
-    for col in measure_cols:
-        output_arr = np.zeros(arr_len, input_ctable.dtype[col])
-        measure_matrix.append(output_arr)
-    measure_matrix_tup = tuple(measure_matrix)
-    # first create an overview of which row goes where (fill the index array)
-    i = 0
+    # process the rows
     for row in input_ctable.iter(outcols=outcols):
         # lookup values and create hash
         groupby_tup = row[0:groupby_cols_len]
         measure_tup = row[groupby_cols_len:]
-        current_hash = hash(groupby_tup)
-        # if the current_hash is unlike the previous hash, lookup the new index value (to speed up sorted arrays)
-        if current_hash != previous_hash:
-            # check if it already exists as a known value
-            hash_found = False
-            for j in range(hash_max):
-                if current_hash == hash_arr[j]:
-                    current_index = j
-                    hash_found = True
-                    break
-            # if it does not exist yet, add it to the hash_arr
-            if not hash_found:
-                current_index = hash_max
-                hash_arr[current_index] = current_hash
-                hash_max += 1
-                # save the unique groupby column values
-                for j, val in enumerate(groupby_tup):
-                    groupby_matrix_tup[j][current_index] = val
-        # save the values
-        for j, val in enumerate(measure_tup):
-            measure_matrix_tup[j][current_index] += val
-        # get ready for the next row of the loop
+        _dict_update(total_dict, groupby_tup, measure_tup)
+    # now create the output table
+    actual_len = len(total_dict)
+    total_matrix = []
+    for col in outcols:
+        output_arr = np.zeros(actual_len, input_ctable.dtype[col])
+        total_matrix.append(output_arr)
+    # now fill the output table
+    i = 0
+    for groupby_tup, measure_tup in total_dict.iteritems():
+        # save groupby
+        for j in range(groupby_cols_len):
+            total_matrix[j][i] = groupby_tup[j]
+        # save measures
+        for j in range(measure_cols_len):
+            total_matrix[j + groupby_cols_len][i] = measure_tup[j]
+        # prepare for next row
         i += 1
-        previous_hash = current_hash
-    # now create the output data frame
-    outcols = groupby_cols + measure_cols
-    total_matrix = groupby_matrix + measure_matrix
-    # cut the arrays to the actual length
-    for output_arr in total_matrix:
-        output_arr = output_arr[0:hash_max]
     # create the output table
     output_table = bcolz.ctable(columns=total_matrix, names=outcols)
     return output_table
