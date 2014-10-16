30e59f70a924f990af7304c1d8dff590eb5c4f3b
Francesc Elies Henar <felies@visualfabriq.com>
2014-10-16 16:12:36 +0200

    group_sortindexer

--- bcolz/algos.py
+++ bcolz/algos.py
@@ -40,3 +40,35 @@ def factorize_pure2(carray_):
         labels.append(buffer)
 
     return labels, lookup
+
+def groupsort_indexer(labels, reverse):
+    ngroups = len(reverse)
+    # count group sizes, location 0 for NA
+    counts = np.zeros(ngroups + 1, dtype=np.int64)
+    n = len(labels)
+    for i in range(n):
+        print labels[i] +1
+        counts[labels[i] + 1] += 1
+
+    # mark the start of each contiguous group of like-indexed data
+    where = np.zeros(ngroups + 1, dtype=np.int64)
+    for i in range(1, ngroups + 1):
+        where[i] = where[i - 1] + counts[i - 1]
+
+    # this is our indexer
+    result = np.zeros(n, dtype=np.int64)
+    for i in range(n):
+        label = labels[i] + 1
+        result[where[label]] = i
+        where[label] += 1
+    return result, counts
+
+import numpy, random, bcolz, tempfile, shutil
+N = int(1e2)
+a = numpy.fromiter((random.choice(['NY', 'IL', 'OH', 'CA']) for _ in xrange(N)), dtype='S2')
+rootdir = tempfile.mkdtemp(prefix='bcolz-factorize')
+shutil.rmtree(rootdir)
+c = bcolz.carray(a, bcolz.cparams(clevel=5, shuffle=False, cname='blosclz'), rootdir=rootdir)
+labels, reverse = bcolz.carray_ext.factorize_cython(c)
+print groupsort_indexer(labels, reverse)
+shutil.rmtree(rootdir)
