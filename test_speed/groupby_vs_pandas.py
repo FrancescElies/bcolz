7f4e5bc98e8c83cfe5e092b648376e324aa53fc4
Francesc Elies Henar <felies@visualfabriq.com>
2014-10-24 15:47:57 +0200

    test speed, groupby column types (string, int and float)

--- test_speed/groupby_vs_pandas.py
+++ test_speed/groupby_vs_pandas.py
@@ -21,14 +21,14 @@ def ctime(label=""):
 
 # -- Common inputs for groupby --
 projects = [
-{'a1': 'build roads',  'a2': 'CA', 'a3': 2, 'm1':   1, 'm2':  2, 'm3':    3.2},
-{'a1': 'fight crime',  'a2': 'IL', 'a3': 1, 'm1':   2, 'm2':  3, 'm3':    4.1},
-{'a1': 'help farmers', 'a2': 'IL', 'a3': 1, 'm1':   4, 'm2':  5, 'm3':    6.2},
-{'a1': 'help farmers', 'a2': 'CA', 'a3': 3, 'm1':   8, 'm2':  9, 'm3':   10.9},
-{'a1': 'build roads',  'a2': 'CA', 'a3': 3, 'm1':  16, 'm2': 17, 'm3':   18.2},
-{'a1': 'fight crime',  'a2': 'IL', 'a3': 1, 'm1':  32, 'm2': 33, 'm3':   34.3},
-{'a1': 'help farmers', 'a2': 'IL', 'a3': 2, 'm1':  64, 'm2': 65, 'm3':   66.6},
-{'a1': 'help farmers', 'a2': 'AR', 'a3': 3, 'm1': 128, 'm2': 129, 'm3': 130.9}
+{'a1': 'build roads',  'a2': 1.1, 'a3': 2, 'm1':   1, 'm2':  2, 'm3':    3.2},
+{'a1': 'fight crime',  'a2': 1.2, 'a3': 1, 'm1':   2, 'm2':  3, 'm3':    4.1},
+{'a1': 'help farmers', 'a2': 1.2, 'a3': 1, 'm1':   4, 'm2':  5, 'm3':    6.2},
+{'a1': 'help farmers', 'a2': 1.1, 'a3': 3, 'm1':   8, 'm2':  9, 'm3':   10.9},
+{'a1': 'build roads',  'a2': 1.1, 'a3': 3, 'm1':  16, 'm2': 17, 'm3':   18.2},
+{'a1': 'fight crime',  'a2': 1.2, 'a3': 1, 'm1':  32, 'm2': 33, 'm3':   34.3},
+{'a1': 'help farmers', 'a2': 1.2, 'a3': 2, 'm1':  64, 'm2': 65, 'm3':   66.6},
+{'a1': 'help farmers', 'a2': 1.3, 'a3': 3, 'm1': 128, 'm2': 129, 'm3': 130.9}
 ]
 
 N = int(1e6)
