b5f848a744225714b47ab8b491ea344b14cb283a
Francesc Elies Henar <felies@visualfabriq.com>
2014-10-27 16:21:24 +0100

    pvm_fact_nielsen_data

--- test_speed/fc_nl_fact_ex_groupby.py
+++ test_speed/fc_nl_fact_ex_groupby.py
@@ -4,11 +4,11 @@ import bcolz
 import pandas as pd
 
 
-file_h5='fc_nl_fact_ex_factory_actuals_2.h5'
+file_h5='pvm_fact_nielsen_data.h5'
 
 hdf = pd.HDFStore(file_h5, mode='r')
 
-fact_df = hdf['/store_0'][ [ 'a_n_11', 'a_n_101' , 'a_n_21' , 'a_n_31', 'm101101'] ]
+fact_df = hdf['/store_0'][ [ 'a_n_11', 'a_n_101' , 'a_n_21' , 'a_n_31', 'm__n_1001'] ]
 prefix = 'bcolz-TestH5'
 rootdir = tempfile.mkdtemp(prefix=prefix)
 os.rmdir(rootdir)  # tests needs this cleared
