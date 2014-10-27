91a257903edee9a73899ee53494e686b56d704f1
Francesc Elies Henar <felies@visualfabriq.com>
2014-10-27 17:34:36 +0100

    pvm_fact_nielsen_data

--- test_speed/fc_nl_fact_ex_groupby.py
+++ test_speed/fc_nl_fact_ex_groupby.py
@@ -8,7 +8,7 @@ file_h5='pvm_fact_nielsen_data.h5'
 
 hdf = pd.HDFStore(file_h5, mode='r')
 
-fact_df = hdf['/store_0'][ [ 'a_n_11', 'a_n_101' , 'a_n_21' , 'a_n_31', 'm__n_1001'] ]
+fact_df = hdf['/store_0'][ [ 'a_n_11', 'a_n_21' , 'a_n_31', 'm_n_1001'] ]
 prefix = 'bcolz-TestH5'
 rootdir = tempfile.mkdtemp(prefix=prefix)
 os.rmdir(rootdir)  # tests needs this cleared
@@ -22,3 +22,6 @@ fact_bcolz.flush()
 fact_bcolz.cache_factor([ 'a_n_11', 'a_n_101' , 'a_n_21' , 'a_n_31'])
 
 %prun -l 20 -s cumulative fact_bcolz.groupby(['a_n_11', 'a_n_21'], ['m101101'], method=2)
+
+x = bcolz.ctable(rootdir='pvm_fact_nielsen_data.bcolz', mode='r')
+x.groupby([ 'a_n_11'], ['m_n_1001'])
